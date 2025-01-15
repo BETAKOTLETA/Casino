6e1cec9a56f7bca0baebdd963f168ab5dfc4fb21
BETAKOTLETA <viktor.oleniuk@gmail.com>
2025-01-15 21:09:15 +0200

    Add login page and MySQL

---
+++ Casino/Login_Form.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Casino
+{
+    partial class Login_Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            Login_account_button = new Button();
+            LoginName_TextBox = new TextBox();
+            LoginPassword_TextBox = new TextBox();
+            Create_account_button = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(320, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(110, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome to Casino";
+            // 
+            // Login_account_button
+            // 
+            Login_account_button.Location = new Point(387, 250);
+            Login_account_button.Name = "Login_account_button";
+            Login_account_button.Size = new Size(75, 23);
+            Login_account_button.TabIndex = 1;
+            Login_account_button.Text = "Login";
+            Login_account_button.UseVisualStyleBackColor = true;
+            Login_account_button.Click += Login_account_button_Click;
+            // 
+            // LoginName_TextBox
+            // 
+            LoginName_TextBox.Location = new Point(330, 114);
+            LoginName_TextBox.Name = "LoginName_TextBox";
+            LoginName_TextBox.Size = new Size(100, 23);
+            LoginName_TextBox.TabIndex = 2;
+            // 
+            // LoginPassword_TextBox
+            // 
+            LoginPassword_TextBox.Location = new Point(330, 187);
+            LoginPassword_TextBox.Name = "LoginPassword_TextBox";
+            LoginPassword_TextBox.Size = new Size(100, 23);
+            LoginPassword_TextBox.TabIndex = 3;
+            // 
+            // Create_account_button
+            // 
+            Create_account_button.Location = new Point(282, 250);
+            Create_account_button.Name = "Create_account_button";
+            Create_account_button.Size = new Size(75, 23);
+            Create_account_button.TabIndex = 4;
+            Create_account_button.Text = "Create";
+            Create_account_button.UseVisualStyleBackColor = true;
+            Create_account_button.Click += Create_account_button_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(330, 81);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Username";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(330, 158);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Password";
+            // 
+            // Login_Form
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(Create_account_button);
+            Controls.Add(LoginPassword_TextBox);
+            Controls.Add(LoginName_TextBox);
+            Controls.Add(Login_account_button);
+            Controls.Add(label1);
+            Name = "Login_Form";
+            Text = "Form1";
+            Load += Login_Form_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button Login_account_button;
+        private TextBox LoginName_TextBox;
+        private TextBox LoginPassword_TextBox;
+        private Button Create_account_button;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ Casino/Login_Form.cs
@@ -0,0 +1,131 @@
+using MySql.Data.MySqlClient;
+using System.Text;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Security.Cryptography;
+
+namespace Casino
+{
+    public partial class Login_Form : Form
+    {
+        public Login_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Form_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Server=192.168.0.6;Database=casinodatabase;User ID=root;Password=123123;";
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    MessageBox.Show("Connection to server established.");
+
+                    string createTableQuery = @"
+                    CREATE TABLE IF NOT EXISTS Users (
+                        Id INT AUTO_INCREMENT PRIMARY KEY,
+                        Username VARCHAR(255) NOT NULL,
+                        PasswordHash VARCHAR(255) NOT NULL
+                    );";
+                    using (MySqlCommand command = new MySqlCommand(createTableQuery, connection))
+                    {
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Table Users created or already exists.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connection error: " + ex.Message);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Create_account_button_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=192.168.0.6;Database=casinodatabase;User ID=root;Password=123123;";
+
+            string userName = LoginName_TextBox.Text;
+            string userPassword = HashPassword(LoginPassword_TextBox.Text);
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO Users (Username, PasswordHash) VALUES (@Username, @PasswordHash)";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Username", userName);
+                        command.Parameters.AddWithValue("@PasswordHash", userPassword);
+                        command.Parameters.AddWithValue("@Money", 2000);
+
+                        command.ExecuteNonQuery();
+                    }
+
+                    MessageBox.Show("Account created successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+        }
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+
+        private void Login_account_button_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=localhost;Database=casinodatabase;User ID=root;Password=123123;";
+            string userName = LoginName_TextBox.Text;
+            string userPassword = HashPassword(LoginPassword_TextBox.Text);
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "SELECT COUNT(*) FROM Users WHERE UserName = @UserName AND PasswordHash = @PasswordHash";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@UserName", userName);
+                        command.Parameters.AddWithValue("@PasswordHash", userPassword);
+
+                        int userCount = Convert.ToInt32(command.ExecuteScalar());
+
+                        if (userCount > 0)
+                        {
+                            MessageBox.Show("Login successful!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid username or password.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+
+    }
+}
